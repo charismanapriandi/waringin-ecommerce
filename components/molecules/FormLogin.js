c2d1ca537f3c08766869a2a1f9ec4e453a9b90a4
rismantrdr <rismantrdr@gmail.com>
2021-07-09 22:13:38 +0700

    create a product detile component, login page and fix some bug

--- components/molecules/FormLogin.js
+++ components/molecules/FormLogin.js
@@ -15,9 +15,7 @@ const FormLogin = () => {
     }
 
     return (
-        <form 
-            onSubmit={handleSubmit(onLogin)}
-            className="w-96 ring ring-background-800 p-4 rounded-xl">
+        <div className="w-96 ring ring-background-800 p-4 rounded-xl">
             <div className="flex justify-between items-center">
                 <p className="cursor-pointer text-sm">
                     <FontAwesomeIcon icon={faChevronLeft}/>
@@ -28,52 +26,54 @@ const FormLogin = () => {
                 <p>Masuk</p>
             </div>
             <Gap height={20} />
-            <div>
-                <Input
-                    {...register('email', {
-                        required: true,
-                        pattern: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-                    })} 
-                    placeholder="email" />
-                    {errors.email?.type === 'required' && 
-                        <Error>Email tidak boleh kosong</Error>
-                    }
-                    {errors.email?.type === 'pattern' && 
-                        <Error>Email tidak valid</Error>
-                    }
-                    <Gap height={5} />
-                <Input
-                    {...register('password', {
-                        required: true,
-                        minLength: 6
-                    })}
-                    placeholder="password" 
-                    password={true}/>
-                    {errors.password?.type === 'required' && 
-                        <Error>Password harus diisi</Error>
-                    } 
-                    {errors.password?.type === 'minLength' && 
-                        <Error>Pasword harus lebih dari 6 huruf</Error>
-                    } 
-                <Gap height={20} />
-                <Button active={true}>Masuk</Button>
-            </div>
-            <Gap height={10} />
-            <p className="text-center">atau masuk dengan</p>
-            <Gap height={10} />
-            <Button type="submit">
-                <FontAwesomeIcon icon={faGoogle} />
-                <span className="ml-4">Google</span>
-            </Button>
-            {/* <Gap height={10} /> */}
-            {/* <div>
-                <p 
-                    className="text-sm">
-                    Belum punya akun? 
-                    <span className="hover:text-background-active cursor-pointer ml-2 transition-all duration-300">DAFTAR</span>
-                </p>
-            </div> */}
-        </form>
+            <form onSubmit={handleSubmit(onLogin)}>
+                <div>
+                    <Input
+                        {...register('email', {
+                            required: true,
+                            pattern: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+                        })} 
+                        placeholder="email" />
+                        {errors.email?.type === 'required' && 
+                            <Error>Email tidak boleh kosong</Error>
+                        }
+                        {errors.email?.type === 'pattern' && 
+                            <Error>Email tidak valid</Error>
+                        }
+                        <Gap height={5} />
+                    <Input
+                        {...register('password', {
+                            required: true,
+                            minLength: 6
+                        })}
+                        placeholder="password" 
+                        password={true}/>
+                        {errors.password?.type === 'required' && 
+                            <Error>Password harus diisi</Error>
+                        } 
+                        {errors.password?.type === 'minLength' && 
+                            <Error>Pasword harus lebih dari 6 huruf</Error>
+                        } 
+                    <Gap height={20} />
+                    <Button active={true}>Masuk</Button>
+                </div>
+                <Gap height={10} />
+                <p className="text-center">atau masuk dengan</p>
+                <Gap height={10} />
+                <Button type="submit">
+                    <FontAwesomeIcon icon={faGoogle} />
+                    <span className="ml-4">Google</span>
+                </Button>
+                {/* <Gap height={10} /> */}
+                {/* <div>
+                    <p 
+                        className="text-sm">
+                        Belum punya akun? 
+                        <span className="hover:text-background-active cursor-pointer ml-2 transition-all duration-300">DAFTAR</span>
+                    </p>
+                </div> */}
+            </form>
+        </div>
     )
 }
 
