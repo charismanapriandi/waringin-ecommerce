683077ba841dee751c59e2b0134b978b458b6503
rismantrdr <rismantrdr@gmail.com>
2021-07-13 18:47:24 +0700

    improve responsive

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from '../views/Home';
-import { Layout, MyScrollbar } from '../components';
+import { Layout, MyScrollbar, ScrollBar } from '../components';
 import Cart from '../views/Cart';
 import Wishlist from '../views/Wishlist';
 import { useEffect } from 'react';
 import { getAllCart, getAllCategory, getAllProduct, getAllWishlist } from '../store/actions/memoryAction';
 import { useDispatch } from 'react-redux';
+import useDevice from '../hook/useDevice';
+import { closeFilter, openFilter } from '../store/actions/statusAction';
 
 export default function App() {
   const dispatch = useDispatch()
-  
+  const device = useDevice()
   // save all data to memory 
   useEffect(() => {
     dispatch(getAllProduct())
@@ -17,18 +19,25 @@ export default function App() {
     dispatch(getAllCart())
     dispatch(getAllWishlist())
   }, [dispatch])
-  
+
+  useEffect(() => {
+    if (window.innerWidth <= 1024) {
+      dispatch(closeFilter())
+    } else {
+      dispatch(openFilter())
+    }
+  }, [])
+
   return (
     <Router>
       <div>
-        {/* <MyScrollbar /> */}
-        <Layout>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/wishlist" component={Wishlist} />
-          </Switch>
-        </Layout>
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/cart" component={Cart} />
+              <Route path="/wishlist" component={Wishlist} />
+            </Switch>
+          </Layout>
       </div>
     </Router>
   )
